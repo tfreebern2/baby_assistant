6598ee7b8e763a99b29e7687eb02556a39c55838
Timothy Freebern II <tfreebern2@gmail.com>
2019-04-26 23:47:32 -0400

    Setup Ate Activity class

--- lib/model/ate_activity.dart
+++ lib/model/ate_activity.dart
@@ -1,31 +1,80 @@
-enum Unit { ounce }
+import 'package:baby_assistant/model/child.dart';
+import 'package:flutter/material.dart';
 
-const Map<Unit, String> unitsOfMeasurement = {Unit.ounce: 'ounce(s)'};
+//enum Unit { ounce }
+//
+//const Map<Unit, String> unitsOfMeasurement = {Unit.ounce: 'ounce(s)'};
 
 class AteActivity {
-  AteActivity({this.date, this.startTime, this.endTime, this.amount, this.unit, this.type});
+  AteActivity(
+    this._date,
+    this._startTime,
+    this._endTime,
+    this._description,
+    this._amount,
+  );
 
-  // id
-  int id;
+  int _id;
+  int _childId;
+  String _date;
+  String _startTime;
+  String _endTime;
+  String _description;
+  String _amount;
 
-  // date of eating
-  DateTime date;
+//  Child child;
 
-  // start of eating
-  DateTime startTime;
+//  // unit of measurement
+//  Unit unit;
+//
+//  // type of food
+//  String type;
 
-  // end of eating
-  DateTime endTime;
+  AteActivity.map(dynamic obj) {
+    this._id = obj["id"];
+    this._childId = obj["childId"];
+    this._date = obj["date"];
+    this._startTime = obj["start_time"];
+    this._endTime = obj["end_time"];
+    this._description = obj["description"];
+    this._amount = obj["amount"];
+  }
 
-  // numerical amount eaten
-  int amount;
+  // Getters
+  int get id => _id;
+  int get childId => _childId;
+  String get date => _date;
+  String get startTime => _startTime;
+  String get endTime => _endTime;
+  String get description => _description;
+  String get amount => _amount;
 
-  // unit of measurement
-  Unit unit;
+  Map<String, dynamic> toMap() {
+    var map = new Map<String, dynamic>();
 
-  // type of food
-  String type;
+    if (_id != null) {
+      map["id"] = _id;
+    }
+
+    map["childId"] = _childId;
+    map["date"] = _date;
+    map["start_time"] = _startTime;
+    map["end_time"] = _endTime;
+    map["description"] = _description;
+    map["amount"] = _amount;
+
+    return map;
+  }
+
+  AteActivity.fromMap(Map<String, dynamic> map) {
+    this._date = map["date"];
+    this._startTime = map["start_time"];
+    this._endTime = map["end_time"];
+    this._description = map["description"];
+    this._amount = map["amount"];
+//    this._unit = map["unit"];
+    this._childId = map["childId"];
+
+    this._id = map["id"];
+  }
 }
-
-
-
